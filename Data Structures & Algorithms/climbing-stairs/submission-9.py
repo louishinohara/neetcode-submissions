600e5484af0c8e329d1c6e20017fd11a276bb155
Ushio Shinohara <louishinohara@gmail.com>
2026-05-06 20:04:49 -0400

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2: return n
+        i, j = 1, 2
+
+        for z in range(3, n+1):
+            k = i + j
+            print(i,j,k)
+            i, j = j, k
+        return k
